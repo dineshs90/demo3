1579b071b86abd93104f18c2d4e9ba6297eda434
Dinesh <dinesh.shanmugaraj@walmart.com>
2024-08-26 20:18:13 +0530

    first

---
+++ dog.go
@@ -0,0 +1,11 @@
+package dog
+
+func dog() string {
+	return "Hi Erick"
+
+}
+
+func dogs() string {
+	return "Package dog - demo3"
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dineshs90/demo3
+
+go 1.23.0
